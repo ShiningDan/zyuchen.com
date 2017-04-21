7271cee21a03bedb75015d071d49fb12673005f6
ShiningDan <zhuce.zyc@gmail.com>
2017-04-21 22:47:42 +0800

    update blog

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
 let fs = require('fs');
 let path = require('path');
 let marked = require('marked');
+let markdown = require('markdown-js');
 
+console.log(markdown);
 let p = path.join(__dirname, 'md/mongodb-入门.md');
 let fileContent = fs.readFileSync(p).toString('utf-8');
 let writeFile = path.join(__dirname, 'md/mongodb.html');
-let writeContent = marked(fileContent);
+// let writeContent = marked(fileContent);
+let writeContent = markdown.makeHtml(fileContent);
 fs.writeFileSync(writeFile, writeContent);
