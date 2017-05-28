6fab5d40108aa7625918208b4b0cf33f0865c85e
ShiningDan <zhuce.zyc@gmail.com>
2017-05-28 22:04:54 +0800

    change redis fetch 5s to 5m

--- redisFetch.js
+++ redisFetch.js
@@ -64,8 +64,8 @@ try {
   });
 
   var rule = new schedule.RecurrenceRule();
-  // rule.minute = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
-  rule.second = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
+  rule.minute = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
+  // rule.second = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
   schedule.scheduleJob(rule, function(){
     console.log('Redis Fetch Schedule start at:' + new Date());
     fetchFormMongoToRedis(redisClient);
