ac54b74fbd2234166546ad4a9e495215b76ee7ff
ShiningDan <zhuce.zyc@gmail.com>
2017-06-22 12:37:15 +0800

    change ga z query to the last

--- app/controllers/googleAnalystics.js
+++ app/controllers/googleAnalystics.js
@@ -4,19 +4,22 @@ exports.ga = function(req, res) {
   let headers = req.headers;
   delete headers.host;
   let query = req.query;
+  let queryZ = query.z;
+  delete query.z;
   query['uip'] = req.connection.remoteAddress.split(":").reverse()[0];
+  query['z'] = queryZ;
   // console.log(query)
   // console.log(headers);
   // console.log(req.query);
   // console.log(req.connection.remoteAddress);
-  request.get('https://www.google-analytics.com/collect')
+  request.get('http://www.google-analytics.com/collect')
     .query(req.query)
     .set(headers)
     .end((err, data) => {
       if (err) {
         console.log(err);
       } else {
-        console.log(data);        
+        // console.log(data);        
       }
     })
   res.end();
@@ -26,19 +29,22 @@ exports.gar = function(req, res) {
   let headers = req.headers;
   delete headers.host;
   let query = req.query;
+  let queryZ = query.z;
+  delete query.z;
   query['uip'] = req.connection.remoteAddress.split(":").reverse()[0];
+  query['z'] = queryZ;
   // console.log(query)
   // console.log(headers);
   // console.log(req.query);
   // console.log(req.connection.remoteAddress);
-  request.get('https://www.google-analytics.com/r/collect')
+  request.get('http://www.google-analytics.com/r/collect')
     .query(query)
     .set(headers)
     .end((err, data) => {
       if (err) {
         console.log(err);
       } else {
-        console.log(data);        
+        // console.log(data);        
       }
     })
   res.end();
